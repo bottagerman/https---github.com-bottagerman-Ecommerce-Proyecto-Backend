4140b286f99b984b4c096726c142603f9766b73a
bottagerman <botta.german@gmail.com>
2023-05-26 15:23:41 -0300

    Handlebar configuration and utils update

--- src/app.js
+++ src/app.js
@@ -1,12 +1,24 @@
 //@ts-check
 import express from "express";
+import handlebars from "express-handlebars";
 import { routerProducts } from "./routes/products.router.js";
 import { routerCart } from "./routes/cart.router.js";
 import { routerProductsView } from "./routes/products.view.router.js";
+import { __dirname } from "../utils.js";
+
 const app = express();
 const port = 8080;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+
+// HANDLEBARS ENGINE CONFIGURATION
+app.engine("handlebars", handlebars.engine());
+app.set("views", __dirname + "/views");
+app.set("view engine", "handlebars");
+
+// PUBLIC FILES 
+app.use(express.static(__dirname + "/public"));
 
 // ALL MY API ENDPOINTS
 
