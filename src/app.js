fa2a74f514d66f3dde30d9af43c0c12f9ffe4a88
bottagerman <botta.german@gmail.com>
2023-05-26 19:49:46 -0300

    renaming and error's fixed

--- src/app.js
+++ src/app.js
@@ -1,16 +1,14 @@
-//@ts-check
 import express from "express";
 import handlebars from "express-handlebars";
 import { routerProducts } from "./routes/products.router.js";
 import { routerCart } from "./routes/cart.router.js";
 import { routerProductsView } from "./routes/products.view.router.js";
-import { __dirname } from "../utils.js";
+import { __dirname } from "./utils.js";
 
 const app = express();
 const port = 8080;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
 
 // HANDLEBARS ENGINE CONFIGURATION
 app.engine("handlebars", handlebars.engine());
@@ -21,16 +19,13 @@ app.set("view engine", "handlebars");
 app.use(express.static(__dirname + "/public"));
 
 // ALL MY API ENDPOINTS
-
 app.use("/api/products", routerProducts);
 app.use("/api/cart", routerCart);
 
 // ALL MY HTLM ENDPOINTS 
-
-app.use("/view/products", routerProductsView);
+app.use("/views/products", routerProductsView);
 
 // GLOBAL ENDPOINT
-
 app.get("*", (req, res) => {
   return res.status(404).json({
     status: "error",
@@ -40,7 +35,6 @@ app.get("*", (req, res) => {
 });
 
 //APP LISTENER
-
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
